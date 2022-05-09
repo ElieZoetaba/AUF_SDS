df68124dd5950b36abcd065f7671f12f0e53bdbe
Kidchris <christianjosaphat0@gmail.com>
2022-05-09 13:54:21 +0000

    including assets files

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet" >
+    <link rel="stylesheet" href="./assets/cs/all.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <script src="./assets/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
