e72932e25febd726ef6904aa0acb2a41450d3a7c
Kidchris <christianjosaphat0@gmail.com>
2022-05-10 09:14:19 +0000

    finishing the frontend

---
+++ pages/list.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/all.css">
+    <link rel="stylesheet" href="../css/list.css">
+    <link rel="stylesheet" href="../assets/css/icons.css">
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            Univsersite Joseph Ki-Zerbo
+            <a class="navbar-brand" href="../index.php">
+                <img src="../images/main-logo.png" alt="" width="100" height="100" class="d-inline-block align-text-top">
+            </a>
+            Science De Sante(SDS)
+        </div>
+    </nav>
+
+    <div>
+
+        <ul class="main-card ">
+            <li class="card li-card my-3">
+                <div class="card-container">
+                    <div>
+                        <h5 class="card card-title px-3">Nom Prenom</h5>
+                    </div>
+                    <div>
+                        <h5 class="card text-muted px-3"><small>Naisssance</small></h5>
+                    </div>
+                    <div>
+                        <h5 class="card px-3">tuteur :Nom :Prenom :Mail :telephone</h5>
+                    </div>
+                </div>
+                <div class="option my-3">
+                    <span type="button" class=" my-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#029035" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                            <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                        </svg>
+                    </span>
+
+                    <div class="modal fade" id="staticBackdrop">
+                        <div class="modal-dialog modal-sm">
+                            <div class="modal-content">
+                                <div type="button" class="option" data-bs-dismiss="modal">Modifier</div>
+                                <br>
+                                <div type="button" class="option">Supprimer</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="card li-card my-3">
+                <div class="card-container">
+                    <div>
+                        <h5 class="card card-title px-3">Nom Prenom</h5>
+                    </div>
+                    <div>
+                        <h5 class="card text-muted px-3"><small>Naisssance</small></h5>
+                    </div>
+                    <div>
+                        <h5 class="card px-3">tuteur :Nom :Prenom :Mail :telephone</h5>
+                    </div>
+                </div>
+                <div class="option my-3">
+                    <span type="button" class=" my-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#029035" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                            <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                        </svg>
+                    </span>
+
+                    <div class="modal fade" id="staticBackdrop">
+                        <div class="modal-dialog modal-sm">
+                            <div class="modal-content">
+                                <div type="button" class="option" data-bs-dismiss="modal">Modifier</div>
+                                <br>
+                                <div type="button" class="option">Supprimer</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <div class="add d-flex">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+        </svg>
+    </div>
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- pages/student/add.php
+++ pages/student/add.php
@@ -8,7 +8,7 @@
 
     <link href="../../assets/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../../assets/css/all.css">
-    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/student/add.css">
     <title>Hello, world!</title>
 </head>
 
@@ -18,22 +18,38 @@
         <img src="../../images/main-logo.png" alt="logo">
 
         <div class="btn-container">
-            <span class="con-ins btn-connexion  btn">Connexion</span> <span class="greeny"> /</span>
-            <span class="con-ins btn-inscription  btn">Inscription</span>
+            <span class="con-ins btn-connexion  btn">Ajouter un Etudiant</span>
         </div>
 
         <div class="inscription">
-            <input type="text" name="nom" id="nom-inscription" placeholder="Nom et Prenom" class="mt-3 px-2">
-            <input type="email" name="email" id="email-inscription" placeholder="Email" class="mt-3 px-2">
-            <input type="password" name="password" id="password-inscription" placeholder="Mot de passe " class="mt-3 px-2">
-            <input type="password" name="password-repeat" id="password-repeat-inscription" placeholder="Repetez le mot de passe" class="mt-3 px-2">
+            <div class="row">
+                <div class="col-5">
+                    <label for="nom"> Nom</label>
+                    <input type="text" name="nom" id="nom-inscription" placeholder="Nom" class="mt-3 px-2 col-6">
+                </div>
+                <div class="col-6">
+                    <label for="prenom"> Prenom</label>
+                    <input type="text" name="prenom" id="prenom-inscription" placeholder="prenom" class="mt-3 px-2 col-6">
+                </div>
+            </div>
+            <div class="col">
+                <label for="email"> Email</label> <br>
+                <input type="email" name="email" id="email-inscription" placeholder="Email" class="mt-3 px-2">
+            </div>
+            <label for="naissance">Date de naissance</label></br>
+            <input type="date" name="naissance" placeholder="Date de naissance" class="mt-3 px-2">
+            <label for="telephone"> Telephone</label>
+            <input type="text" name="telephone" id="" placeholder="Telephone" class="mt-3 px-2"><br>
+            <label for="telephone"> Tuteur</label><br>
+            <input type="text" name="tuteur" id="" placeholder="Tuteur" class="mt-3 px-2">
 
             <div class="submit-container">
-                <input type="submit" value="S'inscrire" class="submit mt-3 mb-3  btn">
+                <input type="submit" value="Enregistrer" class="submit mt-3 mb-3  btn">
             </div>
         </div>
     </form>
 
+    <div id="tri"></div>
 </body>
 
 </html>
